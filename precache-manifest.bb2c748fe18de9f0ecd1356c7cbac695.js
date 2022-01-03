8098ff5ae417d90badf75afcb17ad17b9d9192bd
Thomas Ferreira <weltonthomasferreira@outlook.com>
2022-01-03 19:59:55 -0300

    Updates

---
+++ precache-manifest.bb2c748fe18de9f0ecd1356c7cbac695.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d659f8230a67e2a23a68033b5cbe020",
+    "url": "/trybe-project-trivia-react-redux/index.html"
+  },
+  {
+    "revision": "1cf26b0b2273f2e79bdd",
+    "url": "/trybe-project-trivia-react-redux/static/css/main.8472382b.chunk.css"
+  },
+  {
+    "revision": "77fa3891b85ecc634c3f",
+    "url": "/trybe-project-trivia-react-redux/static/js/2.78b2f304.chunk.js"
+  },
+  {
+    "revision": "a38179c3a123c4a544eacdbb77f01e24",
+    "url": "/trybe-project-trivia-react-redux/static/js/2.78b2f304.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1cf26b0b2273f2e79bdd",
+    "url": "/trybe-project-trivia-react-redux/static/js/main.d0f256bc.chunk.js"
+  },
+  {
+    "revision": "f5479919b3f3eb330952",
+    "url": "/trybe-project-trivia-react-redux/static/js/runtime-main.dd5c3c42.js"
+  }
+]);
